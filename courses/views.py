9348c8b4449ed8ec548f55956d36bbd8a93f4091
Prabesh Subedi <prabeshsharma987@gmail.com>
2018-06-07 01:25:33 -0700

    upto 0303 -correct

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Course
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Course
+
+
+def course_list(request):
+    courses=Course.objects.all()
+    output =','.join([str(course) for course in courses])
+    return HttpResponse(output)
--- learning_site/urls.py
+++ learning_site/urls.py
@@ -1,8 +1,11 @@
+from django.conf.urls import include, url
 from django.contrib import admin
-from django.conf.urls import  include,url
-from .import views
+
+from . import views
+
 urlpatterns = [
-    url('admin/', admin.site.urls),
-    url(r'^$',views.hello_world),
+    url(r'^courses/', include('courses.urls')),
+        url('admin/', admin.site.urls),
+        url(r'^$', views.hello_world),
 
 ]
